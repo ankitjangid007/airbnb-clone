fe6b27cadcac7424604832cf8c227076bd0064d8
ankitjangid007 <ankitjangid2021@gmail.com>
2023-06-01 15:28:00 +0530

    single listing view

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Container from "../Container";
+import { usePathname, useSearchParams } from "next/navigation";
 import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 import {
   GiBarn,
@@ -16,8 +16,9 @@ import { FaSkiing } from "react-icons/fa";
 import { BsSnow } from "react-icons/bs";
 import { IoDiamond } from "react-icons/io5";
 import { MdOutlineVilla } from "react-icons/md";
+
 import CategoryBox from "../CategoryBox";
-import { usePathname, useSearchParams } from "next/navigation";
+import Container from "../Container";
 
 export const categories = [
   {
@@ -100,18 +101,19 @@ export const categories = [
 const Categories = () => {
   const params = useSearchParams();
   const category = params?.get("category");
-
   const pathname = usePathname();
   const isMainPage = pathname === "/";
 
-  // if (isMainPage) return null;
+  if (!isMainPage) {
+    return null;
+  }
 
   return (
     <Container>
-      <div className="flex flex-row items-center justify-between pt-4 overflow-hidden overflow-x-auto">
-        {categories.map((item, i) => (
+      <div className="flex flex-row items-center justify-between pt-4 overflow-x-auto ">
+        {categories.map((item) => (
           <CategoryBox
-            key={i}
+            key={item.label}
             label={item.label}
             icon={item.icon}
             selected={category === item.label}
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -14,7 +14,12 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed z-10 w-full bg-white shadow-sm">
-      <div className="py-4 border-b-[1px]">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />

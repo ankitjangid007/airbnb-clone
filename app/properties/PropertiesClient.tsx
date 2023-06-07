fa4e01ad3c68dea5d490bc9e03acc660baa02b53
ankitjangid007 <ankitjangid2021@gmail.com>
2023-06-07 15:47:06 +0530

    bug fixes

--- app/properties/PropertiesClient.tsx
+++ app/properties/PropertiesClient.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { toast } from "react-hot-toast";
 import axios from "axios";
@@ -12,67 +12,55 @@ import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 
 interface PropertiesClientProps {
-  listings: SafeListing[],
-  currentUser?: SafeUser | null,
+  listings: SafeListing[];
+  currentUser?: SafeUser | null;
 }
 
 const PropertiesClient: React.FC<PropertiesClientProps> = ({
   listings,
-  currentUser
+  currentUser,
 }) => {
   const router = useRouter();
-  const [deletingId, setDeletingId] = useState('');
+  const [deletingId, setDeletingId] = useState("");
 
-  const onDelete = useCallback((id: string) => {
-    setDeletingId(id);
+  const onDelete = useCallback(
+    (id: string) => {
+      setDeletingId(id);
 
-    axios.delete(`/api/listings/${id}`)
-    .then(() => {
-      toast.success('Listing deleted');
-      router.refresh();
-    })
-    .catch((error) => {
-      toast.error(error?.response?.data?.error)
-    })
-    .finally(() => {
-      setDeletingId('');
-    })
-  }, [router]);
+      axios
+        .delete(`/api/listings/${id}`)
+        .then(() => {
+          toast.success("Listing deleted");
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error);
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
 
-
-  return ( 
+  return (
     <Container>
-      <Heading
-        title="Properties"
-        subtitle="List of your properties"
-      />
-      <div 
-        className="
-          mt-10
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
-      >
+      <Heading title="Properties" subtitle="List of your properties" />
+      <div className="grid grid-cols-1 gap-8 mt-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {listings.map((listing: any) => (
           <ListingCard
             key={listing.id}
             data={listing}
             actionId={listing.id}
             onAction={onDelete}
-            disabled={deletingId === listing.id}
+            disable={deletingId === listing.id}
             actionLabel="Delete property"
             currentUser={currentUser}
           />
         ))}
       </div>
     </Container>
-   );
-}
- 
+  );
+};
+
 export default PropertiesClient;

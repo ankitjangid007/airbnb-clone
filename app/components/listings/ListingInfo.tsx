2d873d9c81ff6d737e5a9c734ba1776bcfe92939
ankitjangid007 <ankitjangid2021@gmail.com>
2023-06-07 11:46:23 +0530

    bug resolve

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -46,16 +46,14 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
     <div className="flex flex-col col-span-4 gap-8">
       <div className="flex flex-row items-center justify-between">
         <div className="flex flex-col gap-1">
-          <div className="text-xl font-semibold">
-            Hosted by {user?.user?.name}
-          </div>
+          <div className="text-xl font-semibold">Hosted by {user?.name}</div>
           <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
             <div>{guestCount} guests</div>
             <div>{roomCount} rooms</div>
             <div>{bathroomCount} bathrooms</div>
           </div>
         </div>
-        <Avatar src={user?.user?.image} width={50} />
+        <Avatar src={user?.image} width={50} />
       </div>
       <hr />
       {category && (
